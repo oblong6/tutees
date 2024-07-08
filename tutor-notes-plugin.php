c16c7c7853009e302a0bc7197659e0dde694c2a4
oblong6 <40916713+oblong6@users.noreply.github.com>
2024-07-08 13:58:30 +0100

    Update tutor-notes-plugin.php

--- tutor-notes-plugin.php
+++ tutor-notes-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Tutor Notes Plugin
  * Description: A plugin to store and display introductory session notes for tutees.
- * Version: 1.6
+ * Version: 1.7
  * Author: Liam Jordan - www.liamjordan.co.uk
  */
 
